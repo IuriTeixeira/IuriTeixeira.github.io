08f3737726b6bf865b19fa0d42343ef905ad3af7
Iuri Teixeira <irt@icomp.ufam.edu.br>
2024-11-29 11:58:52 -0400

    finished swords.json, added TableComponent.js

--- src/app/weapons/page.js
+++ src/app/weapons/page.js
@@ -1,7 +1,13 @@
+import TableComponent from "./components/TableComponent"
+import swords from './swords.json'
+
 export default function Weapons() {
+    const getHeadings = () => {
+        return Object.keys(swords[0]);
+    }
     return (
         <>
-            <h1>Hello, Next.js!</h1>
+            <TableComponent theadData={getHeadings()} tbodyData={swords}/> 
         </>
     )
-  }
+}
