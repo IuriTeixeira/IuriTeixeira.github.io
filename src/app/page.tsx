c422172151d67ce4756e146439fb9afd594898d5
Iuri Teixeira <irt@icomp.ufam.edu.br>
2025-02-07 11:47:12 -0400

    attempt at adding stats to front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { useLanguageContext } from "./language-provider";
 import { v4 as uuidv4 } from 'uuid';
 import displayResistance from './components/displayResistance';
 import displayStat from './components/displayStat';
+import raceStats from './races.json'
+import classStats from './classes.json'
 
 export default function Home() {
     const language = useLanguageContext()
@@ -17,16 +19,34 @@ export default function Home() {
     const [rear, setRear] = useState<string>('Rear / Cras Dyne')
     const [arm, setArm] = useState<string>('Rear / Cras Noom')
     const [leg, setLeg] = useState<string>('Rear / Cras Ment')
-    const [hp, setHp] = useState<number>(650)
+    const [baseHp, setBaseHp] = useState<number>(650)
+    const [bonusHp, setBonusHp] = useState<number>(0)
+    const [totalHp, setTotalHp] = useState<number>(650)
     const [pp, setPp] = useState<number>(120)
-    const [sAtk, setSAtk] = useState<number>(540)
-    const [rAtk, setRAtk] = useState<number>(540)
-    const [tAtk, setTAtk] = useState<number>(540)
-    const [dex, setDex] = useState<number>(415)
-    const [sDef, setSDef] = useState<number>(450)
-    const [rDef, setRDef] = useState<number>(450)
-    const [tDef, setTDef] = useState<number>(450)
+    const [baseSAtk, setBaseSAtk] = useState<number>(540)
+    const [bonusSAtk, setBonusSAtk] = useState<number>(0)
+    const [totalSAtk, setTotalSAtk] = useState<number>(540)
+    const [baseRAtk, setBaseRAtk] = useState<number>(540)
+    const [bonusRAtk, setBonusRAtk] = useState<number>(0)
+    const [totalRAtk, setTotalRAtk] = useState<number>(540)
+    const [baseTAtk, setBaseTAtk] = useState<number>(540)
+    const [bonusTAtk, setBonusTAtk] = useState<number>(0)
+    const [totalTAtk, setTotalTAtk] = useState<number>(540)
+    const [baseDex, setBaseDex] = useState<number>(415)
+    const [bonusDex, setBonusDex] = useState<number>(0)
+    const [totalDex, setTotalDex] = useState<number>(415)
+    const [baseSDef, setBaseSDef] = useState<number>(450)
+    const [bonusSDef, setBonusSDef] = useState<number>(0)
+    const [totalSDef, setTotalSDef] = useState<number>(450)
+    const [baseRDef, setBaseRDef] = useState<number>(450)
+    const [bonusRDef, setBonusRDef] = useState<number>(0)
+    const [totalRDef, setTotalRDef] = useState<number>(450)
+    const [baseTDef, setBaseTDef] = useState<number>(450)
+    const [bonusTDef, setBonusTDef] = useState<number>(0)
+    const [totalTDef, setTotalTDef] = useState<number>(450)
 
+    let raceData: any = raceStats.find(selectedRace => selectedRace[`Name (English)`] === race)
+    let mainClassData: any = classStats.find(selectedClass => selectedClass[`Name (English)`] === mainClass)
     const raceList: string[] = ['Human Male', 'Human Female', 'Newman Male', 'Newman Female', 'CAST Male', 'Cast Female', 'Dewman Male', 'Dewman Female']
     const magTypeList: string[] = ['S-ATK', 'R-ATK', 'T-ATK', 'DEX']
     const mainClassList: string[] = ['Hunter', 'Ranger', 'Force', 'Fighter', 'Gunner', 'Techer', 'Summoner', 'Hero', 'Phantom', 'Etoile', 'Luster']
@@ -71,20 +91,20 @@ export default function Home() {
         default:
             return (
                 <>
-                    <h1>PSO2 Character Simulator</h1>
-                    <Table withColumnBorders>
+                    <Flex justify="center" align="center" key={uuidv4()} gap={5}><h1>PSO2 Character Simulator</h1></Flex>
+                    <Table withColumnBorders w='70%' align='center'>
                         <Table.Thead>
                             <Table.Tr>
-                                <Table.Th colSpan={10}><Flex justify='center'>Character Sheet</Flex></Table.Th>
+                                <Table.Th colSpan={10}><Flex justify='center'>CHARACTER SHEET</Flex></Table.Th>
                             </Table.Tr>
                         </Table.Thead>
                         <Table.Tbody>
                             <Table.Tr>
-                                <Table.Td rowSpan={2}><Flex justify='center'><Image fallbackSrc='/Blank.png' key={uuidv4()} src={`/portraits/${race}.png`} alt="S-ATK" w={62} h={62} /></Flex></Table.Td>
                                 <Table.Th>Race</Table.Th>
                                 <Table.Td>
                                     <Combobox store={raceCombobox} onOptionSubmit={(val) => {
                                         setRace(val);
+                                        setTotalHp(baseHp*raceData["HP"]*mainClassData["HP"])
                                         raceCombobox.closeDropdown();
                                     }}>
                                         <Combobox.Target>
@@ -187,7 +207,7 @@ export default function Home() {
                             </Table.Tr>
                         </Table.Tbody>
                     </Table>
-                    <Table withColumnBorders>
+                    <Table withColumnBorders w='70%' align='center'>
                         <Table.Thead>
                             <Table.Tr>
                                 <Table.Th colSpan={10}><Flex justify='center'>Gear</Flex></Table.Th>
@@ -212,10 +232,10 @@ export default function Home() {
                             </Table.Tr>
                         </Table.Tbody>
                     </Table>
-                    <Table withColumnBorders>
+                    <Table withColumnBorders w='70%' align='center'>
                         <Table.Thead>
                             <Table.Tr>
-                                <Table.Th colSpan={10}><Flex justify='center'>Character Sheet</Flex></Table.Th>
+                                <Table.Th colSpan={10}><Flex justify='center'>SKILLS</Flex></Table.Th>
                             </Table.Tr>
                         </Table.Thead>
                         <Table.Tbody>
@@ -224,52 +244,52 @@ export default function Home() {
                             </Table.Tr>
                         </Table.Tbody>
                     </Table>
-                    <Table withColumnBorders>
+                    <Table withColumnBorders w='70%' align='center'>
                         <Table.Thead>
                             <Table.Tr>
-                                <Table.Th colSpan={10}><Flex justify='center'>Stats</Flex></Table.Th>
+                                <Table.Th colSpan={10}><Flex justify='center'>STATS</Flex></Table.Th>
                             </Table.Tr>
                         </Table.Thead>
                         <Table.Tbody>
                             <Table.Tr>
-                                <Table.Th colSpan={2}><Flex justify="center" align="center" key={uuidv4()} gap={5}>HP</Flex></Table.Th>
-                                <Table.Th colSpan={2}><Flex justify="center" align="center" key={uuidv4()} gap={5}>PP</Flex></Table.Th>
+                                <Table.Th><Flex justify="center" align="center" key={uuidv4()} gap={5}>HP</Flex></Table.Th>
+                                <Table.Th><Flex justify="center" align="center" key={uuidv4()} gap={5}>PP</Flex></Table.Th>
+                                <Table.Th><Flex justify="center" align="center" key={uuidv4()} gap={5}>DEX</Flex></Table.Th>
                             </Table.Tr>
                             <Table.Tr>
-                                <Table.Td colSpan={2}><Flex justify="center" align="center" key={uuidv4()} gap={5}>{hp}</Flex></Table.Td>
-                                <Table.Td colSpan={2}><Flex justify="center" align="center" key={uuidv4()} gap={5}>{pp}</Flex></Table.Td>
+                                <Table.Td><Flex justify="center" align="center" key={uuidv4()} gap={5}>{Math.floor(totalHp)}</Flex></Table.Td>
+                                <Table.Td><Flex justify="center" align="center" key={uuidv4()} gap={5}>{pp}</Flex></Table.Td>
+                                <Table.Td>
+                                    <Flex justify="center" align="center" key={uuidv4()} gap={5}>
+                                        {magType === 'DEX' && displayStat('DEX', Math.floor(totalDex) + 200)}
+                                        {magType !== 'DEX' && displayStat('DEX', Math.floor(totalDex))}
+                                    </Flex>
+                                </Table.Td>
                             </Table.Tr>
                             <Table.Tr>
                                 <Table.Th><Flex justify="center" align="center" key={uuidv4()} gap={5}>ATK</Flex></Table.Th>
-                                <Table.Th><Flex justify="center" align="center" key={uuidv4()} gap={5}>DEX</Flex></Table.Th>
                                 <Table.Th><Flex justify="center" align="center" key={uuidv4()} gap={5}>DEF</Flex></Table.Th>
                                 <Table.Th><Flex justify="center" align="center" key={uuidv4()} gap={5}>Damage Boosts</Flex></Table.Th>
                             </Table.Tr>
                             <Table.Tr>
                                 <Table.Td>
                                     <Flex justify="center" align="center" key={uuidv4()} gap={5}>
-                                        {magType === 'S-ATK' && displayStat('S-ATK', sAtk + 200)}
-                                        {magType !== 'S-ATK' && displayStat('S-ATK', sAtk)}
+                                        {magType === 'S-ATK' && displayStat('S-ATK', Math.floor(totalSAtk + 200))}
+                                        {magType !== 'S-ATK' && displayStat('S-ATK', Math.floor(totalSAtk))}
                                     </Flex>
                                     <Flex justify="center" align="center" key={uuidv4()} gap={5}>
-                                        {magType === 'R-ATK' && displayStat('R-ATK', rAtk + 200)}
-                                        {magType !== 'R-ATK' && displayStat('R-ATK', rAtk)}
+                                        {magType === 'R-ATK' && displayStat('R-ATK', Math.floor(totalRAtk + 200))}
+                                        {magType !== 'R-ATK' && displayStat('R-ATK', Math.floor(totalRAtk))}
                                     </Flex>
                                     <Flex justify="center" align="center" key={uuidv4()} gap={5}>
-                                        {magType === 'T-ATK' && displayStat('T-ATK', tAtk + 200)}
-                                        {magType !== 'T-ATK' && displayStat('T-ATK', tAtk)}
+                                        {magType === 'T-ATK' && displayStat('T-ATK', Math.floor(totalTAtk + 200))}
+                                        {magType !== 'T-ATK' && displayStat('T-ATK', Math.floor(totalTAtk))}
                                     </Flex>
                                 </Table.Td>
                                 <Table.Td>
-                                    <Flex justify="center" align="center" key={uuidv4()} gap={5}>
-                                        {magType === 'DEX' && displayStat('DEX', dex + 200)}
-                                        {magType !== 'DEX' && displayStat('DEX', dex)}
-                                    </Flex>
-                                </Table.Td>
-                                <Table.Td>
-                                    <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayStat('S-DEF', sDef)}</Flex>
-                                    <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayStat('R-DEF', rDef)}</Flex>
-                                    <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayStat('T-DEF', tDef)}</Flex>
+                                    <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayStat('S-DEF', Math.floor(totalSDef))}</Flex>
+                                    <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayStat('R-DEF', Math.floor(totalRDef))}</Flex>
+                                    <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayStat('T-DEF', Math.floor(totalTDef))}</Flex>
                                 </Table.Td>
                                 <Table.Td>
                                     <Flex justify="center" align="center" key={uuidv4()} gap={0}>{displayStat('S-ATK', 100)}%</Flex>
@@ -278,21 +298,23 @@ export default function Home() {
                                 </Table.Td>
                             </Table.Tr>
                             <Table.Tr>
-                                <Table.Th colSpan={9}><Flex justify="center" align="center" key={uuidv4()} gap={5}>Resistances</Flex></Table.Th>
+                                <Table.Th colSpan={9}><Flex justify="center" align="center" key={uuidv4()} gap={5}>RESISTANCES</Flex></Table.Th>
                             </Table.Tr>
                             <Table.Tr>
-                                <Table.Td colSpan={9}>
-                                    <SimpleGrid cols={3} spacing={0} verticalSpacing={0}>
+                                <Table.Td>
                                         <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayResistance('Strike Resistance', 10)}</Flex>
                                         <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayResistance('Ranged Resistance', 10)}</Flex>
                                         <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayResistance('Tech Resistance', 10)}</Flex>
+                                </Table.Td>
+                                <Table.Td>
                                         <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayResistance('Fire Resistance', 10)}</Flex>
                                         <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayResistance('Ice Resistance', 10)}</Flex>
                                         <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayResistance('Lightning Resistance', 10)}</Flex>
+                                </Table.Td>
+                                <Table.Td>
                                         <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayResistance('Wind Resistance', 10)}</Flex>
                                         <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayResistance('Light Resistance', 10)}</Flex>
                                         <Flex justify="center" align="center" key={uuidv4()} gap={5}>{displayResistance('Dark Resistance', 10)}</Flex>
-                                    </SimpleGrid>
                                 </Table.Td>
                             </Table.Tr>
                         </Table.Tbody>
