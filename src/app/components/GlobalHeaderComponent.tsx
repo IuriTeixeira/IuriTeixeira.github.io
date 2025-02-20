8b428e32a71bf5463d8d15899b4c9ed83b195325
Iuri Teixeira <irt@icomp.ufam.edu.br>
2025-02-20 12:37:40 -0400

    japanese text fixes on home page

--- src/app/components/GlobalHeaderComponent.tsx
+++ src/app/components/GlobalHeaderComponent.tsx
@@ -1,29 +1,31 @@
 'use client'
 
 import React from "react";
-import { Image, Button, Group } from '@mantine/core';
+import { Image, Button, Group, Flex} from '@mantine/core';
 import { useLanguageContext } from "../language-provider";
 import Link from "next/link";
 
 export default function GlobalHeaderComponent() {
     const language = useLanguageContext()
+    let homePage: string
+    let weaponPage: string
+    let unitPage: string
     switch (language.language) {
-        case 'English':
-        case 'Global':
-            return (
-                <Group justify="center" mt="sm">
-                    <Link href="/"><Button><Image src="/favicon.ico" alt="Home" w={16} h={16} /> Home page</Button></Link>
-                    <Link href="/geardata/weapons"><Button><Image src="/icons/Sword.png" alt="Weapons" w={16} h={16} /> Weapons page</Button></Link>
-                    <Link href="/geardata/units"><Button><Image src="/icons/Rear.png" alt="Units" w={16} h={16} /> Units page</Button></Link>
-                </Group>
-            )
         case 'JP':
-            return (
-                <Group justify="center" mt="sm">
-                    <Link href="/"><Button><Image src="/favicon.ico" alt="Home" w={16} h={16} /> Home page</Button></Link>
-                    <Link href="/geardata/weapons"><Button><Image src="/icons/Sword.png" alt="Weapons" w={16} h={16} /> Weapons page</Button></Link>
-                    <Link href="/geardata/units"><Button><Image src="/icons/Rear.png" alt="Units" w={16} h={16} /> Units page</Button></Link>
-                </Group>
-            )
+            homePage = 'ホーム'
+            weaponPage = '武器ページ'
+            unitPage = '防具ページ'
+            break
+        default:
+            homePage = 'Home Page'
+            weaponPage = 'Weapons Page'
+            unitPage = 'Units Page'
     }
+    return (
+        <Group justify="center" mt="sm">
+            <Link href="/"><Button><Flex align='center' justify='center' gap={3}><Image src="/favicon.ico" alt={homePage} w={16} h={16} /> {homePage}</Flex></Button></Link>
+            <Link href="/geardata/weapons"><Button><Flex align='center' justify='center' gap={3}><Image src="/icons/Sword.png" alt={weaponPage} w={16} h={16} /> {weaponPage}</Flex></Button></Link>
+            <Link href="/geardata/units"><Button><Flex align='center' justify='center' gap={3}><Image src="/icons/Rear.png" alt={unitPage} w={16} h={16} /> {unitPage}</Flex></Button></Link>
+        </Group>
+    )
 }
